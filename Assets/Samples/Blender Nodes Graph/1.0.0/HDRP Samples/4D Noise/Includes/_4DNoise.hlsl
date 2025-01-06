313afaff723ac523c0b041cd830d377fde8a265b
Yusuf <yusuf_zeki1234@hotmail.com>
2025-01-06 16:03:06 +0300

    fixed

--- Assets/Samples/Blender Nodes Graph/1.0.0/HDRP Samples/4D Noise/Includes/_4DNoise.hlsl
+++ Assets/Samples/Blender Nodes Graph/1.0.0/HDRP Samples/4D Noise/Includes/_4DNoise.hlsl
@@ -3,9 +3,9 @@
 void _4DNoise_float(float3 _POS, float3 _PVS, float3 _PWS, float3 _NOS, float3 _NVS, float3 _NWS, float3 _NTS, float3 _TWS, float3 _BTWS, float3 _UV, float3 _SP, float3 _VVS, float3 _VWS, out float4 ColorOut)
 {
 	
-	float _Math_39172 = math_multiply(_Time, 3, 0.5);
-	float4 _Mapping_39166 = float4(mapping_point(float4(_POS, 0), float3(0, 0, 0), float3(0, 0, 0), float3(1, 1, 1)), 0);
-	float _SimpleNoiseTexture_39168_fac; float4 _SimpleNoiseTexture_39168_col; node_simple_noise_texture_full(_Mapping_39166, _Math_39172, 5, 2, 0,5, 0, 2, _SimpleNoiseTexture_39168_fac, _SimpleNoiseTexture_39168_col);
+	float _Math_21032 = math_multiply(_Time, 3, 0.5);
+	float4 _Mapping_21028 = float4(mapping_point(float4(_POS, 0), float3(0, 0, 0), float3(0, 0, 0), float3(1, 1, 1)), 0);
+	float _SimpleNoiseTexture_21034_fac; float4 _SimpleNoiseTexture_21034_col; node_simple_noise_texture_full(_Mapping_21028, _Math_21032, 5, 2, 0,5, 0, 2, _SimpleNoiseTexture_21034_fac, _SimpleNoiseTexture_21034_col);
 
-	ColorOut = _SimpleNoiseTexture_39168_col;
+	ColorOut = _SimpleNoiseTexture_21034_col;
 }
--- Assets/Samples/Blender Nodes Graph/1.0.0/HDRP Samples/ExamplePreview/Includes/ExamplePreview.hlsl
+++ Assets/Samples/Blender Nodes Graph/1.0.0/HDRP Samples/ExamplePreview/Includes/ExamplePreview.hlsl
@@ -1,12 +1,12 @@
 #include <Packages/com.blendernodesgraph.core/Editor/Includes/Importers.hlsl>
 
-void ExamplePreview_float(float3 _POS, float3 _PVS, float3 _PWS, float3 _NOS, float3 _NVS, float3 _NWS, float3 _NTS, float3 _TWS, float3 _BTWS, float3 _UV, float3 _SP, float3 _VVS, float3 _VWS, Texture2D curve_31724, out float4 ColorOut)
+void ExamplePreview_float(float3 _POS, float3 _PVS, float3 _PWS, float3 _NOS, float3 _NVS, float3 _NWS, float3 _NTS, float3 _TWS, float3 _BTWS, float3 _UV, float3 _SP, float3 _VVS, float3 _VWS, Texture2D curve_21136, out float4 ColorOut)
 {
 	
-	float _Math_31740 = math_add(_Time, 8, 0.5);
-	float4 _Mapping_31722 = float4(mapping_point(float4(_POS, 0), float3(0, 0, 0), float3(0, 0, 0), float3(1, 1, 1)), 0);
-	float _SimpleNoiseTexture_31720_fac; float4 _SimpleNoiseTexture_31720_col; node_simple_noise_texture_full(_Mapping_31722, _Math_31740, 5, 2, 0.5, 0, 2, _SimpleNoiseTexture_31720_fac, _SimpleNoiseTexture_31720_col);
-	float4 _RGBCurves_31724 = node_rgb_curves(1, _SimpleNoiseTexture_31720_col, curve_31724);
+	float _Math_21126 = math_add(_Time, 8, 0.5);
+	float4 _Mapping_21130 = float4(mapping_point(float4(_POS, 0), float3(0, 0, 0), float3(0, 0, 0), float3(1, 1, 1)), 0);
+	float _SimpleNoiseTexture_21128_fac; float4 _SimpleNoiseTexture_21128_col; node_simple_noise_texture_full(_Mapping_21130, _Math_21126, 5, 2, 0,5, 0, 2, _SimpleNoiseTexture_21128_fac, _SimpleNoiseTexture_21128_col);
+	float4 _RGBCurves_21136 = node_rgb_curves(1, _SimpleNoiseTexture_21128_col, curve_21136);
 
-	ColorOut = _RGBCurves_31724;
+	ColorOut = _RGBCurves_21136;
 }
